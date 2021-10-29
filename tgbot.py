a6c4dd82cd00c29c6af5db14a28b6e60e6700d33
na-kolenke-development <77058806+na-kolenke-development@users.noreply.github.com>
2021-10-29 22:05:35 +0300

    Update tgbot.py

--- tgbot.py
+++ tgbot.py
@@ -39,7 +39,7 @@ def process_msg(func):
 @process_msg
 async def user_based_dialog_former(msg):
     """Формирует чат для ввода в gpt, на основе истории диалога с пользователем"""
-    if not msg.content or len(msg.content) < 1 or msg.content[0] == '/' or re.search("мутя", msg.content.lower()) == False:
+    if not msg.content or len(msg.content) < 1 or msg.content[0] == '/' or bool(re.search("мутя", msg.content.lower())) == False:
         return
     msg.content = filter_symbol(msg.content, ":", " ")
     user_history = add_to_user_history(msg, history)
@@ -64,7 +64,7 @@ async def user_based_dialog_former(msg):
 @process_msg
 async def chat_based_dialog_former(msg):
     """Формирует чат для ввода в gpt, на основе истории диалога в беседе"""
-    if not msg.content or len(msg.content) < 1 or msg.content[0] == '/' or re.search("мутя", msg.content.lower()) == False:
+    if not msg.content or len(msg.content) < 1 or msg.content[0] == '/' or bool(re.search("мутя", msg.content.lower())) == False:
         return
     msg.content = filter_symbol(msg.content, ":", " ")
     chat_history = add_to_chat_history(msg, history)
